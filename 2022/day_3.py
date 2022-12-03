5328bc15ab3bb887b2c80b385d173e66a8e1dc19
Nathan Rutenbeck <nathan@stonesetfarm.com>
2022-12-03 10:03:20 -0500

    day 3 part 2

--- 2022/day_3.py
+++ 2022/day_3.py
@@ -48,4 +48,41 @@ def get_priority_sum(path):
 priorities_total = get_priority_sum('data/day_3.txt')
 print(priorities_total) # 8349
 
+# %% Divide rucksacks into sets of 3
+
+with open('data/day_3.txt') as input:
+    rucksacks = input.read().split("\n")[:-1]
+
+def group_rucksacks(rucksacks, n):
+    for i in range(0, len(rucksacks), n):
+        yield(rucksacks[i : i + n])
+
+grouped_example = list(group_rucksacks(example, 3))
+grouped_rucksacks = list(group_rucksacks(rucksacks, 3))
+
+for group in grouped_rucksacks:
+    assert len(group) == 3
+
+#%% # find and prioritize common item in each group
+
+def prioritize_group_badge(group):
+
+    priorities = dict(
+        zip(string.ascii_lowercase + string.ascii_uppercase, range(1, 53))
+    )
+
+    # find badge
+    badge = list(set(group[0]) & set(group[1]) & set(group[2]))[0]
+
+    return(priorities[badge])
+
+example_badge_priorities = [prioritize_group_badge(g) for g in grouped_example]
+
+assert example_badge_priorities == [18, 52]
+assert sum(example_badge_priorities) == 70
+
+# %% # iterate over groups and sum
+
+badge_priorities = [prioritize_group_badge(g) for g in grouped_rucksacks]
+print(sum(badge_priorities))
 # %%
