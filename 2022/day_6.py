ef9ca001e4a05a9c6cb2ffc4ccc8eae247173a28
Nathan Rutenbeck <nathan@stonesetfarm.com>
2022-12-06 19:35:56 -0500

    day 6

---
+++ 2022/day_6.py
@@ -0,0 +1,62 @@
+#%% example signal
+
+def find_marker(signal_string, signal_length):
+    for s in range(signal_length, len(signal_string) + 1):
+        start = s - signal_length
+        stop = s
+        target = signal_string[start : stop]
+        target_set = set(target)
+        is_marker = len(target_set) == len(target)
+        if is_marker:
+            return s
+
+# %%
+example_signals = [
+    'mjqjpqmgbljsphdztnvjfqwrcgsmlb',
+    'bvwbjplbgvbhsrlpgdmjqwftvncz',
+    'nppdvjthqldpwncqszvftbrmjlhg',
+    'nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg',
+    'zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw'
+]
+
+example_packet_markers = [find_marker(s, 4) for s in example_signals]
+
+assert example_packet_markers == [7, 5, 6, 10, 11]
+        
+# %%
+
+example_message_markers = [find_marker(s, 14) for s in example_signals]
+
+assert example_message_markers == [19, 23, 23, 29, 26]
+
+#%% 
+
+class ElfSignal:
+    
+    def __init__(self, input_path):
+        with open(input_path) as input:
+            signal_string = input.read()
+
+        self.signal_string = signal_string
+
+    def find_marker(self, signal_length):
+        for s in range(signal_length, len(self.signal_string) + 1):
+            start = s - signal_length
+            stop = s
+            target = self.signal_string[start : stop]
+            target_set = set(target)
+            is_marker = len(target_set) == len(target)
+            if is_marker:
+                return s
+
+#%%
+
+signal = ElfSignal(input_path = 'data/day_6.txt')
+# %%
+packet_marker = signal.find_marker(signal_length = 4)
+packet_marker
+
+#%% 
+
+message_marker = signal.find_marker(signal_length = 14)
+message_marker
